35d54bcaca6673a5572d89aa8e10fad715897598
gramaphenia <dicoratojuanpablo@gmail.com>
2023-09-27 23:10:59 -0300

    modificaciones

--- clase-dos/1.http.js
+++ clase-dos/1.http.js
@@ -1,12 +1,12 @@
-const http = require("node:http"); // protocolo HTTP
+const http = require('node:http') // protocolo HTTP
 
-const desiredPort = process.env.PORT ?? 1234;
+const desiredPort = process.env.PORT ?? 1234
 
 const server = http.createServer((req, res) => {
-  console.log("request received");
-  res.end("Haaaaa!!!");
-});
+  console.log('request received: ', req.url)
+  res.end('Haaaaa!!!')
+})
 
-server.listen(disiredPort, () => {
-  console.log(`server listening on port http://localhost:${port}`);
-});
+server.listen(desiredPort, () => {
+  console.log(`server listening on port http://localhost:${desiredPort}`)
+})
