9d7bfd08b6d46703d0860f67685da375e05ae3c2
gramaphenia <dicoratojuanpablo@gmail.com>
2023-10-10 11:26:56 -0300

    mod-fix

---
+++ clase-tres/README.md
@@ -0,0 +1,20 @@
+# REST (ARQUITECTURA DE SOFTWARE)
+
+### Recursos
+
+- En rest todo es considerado un recurso. Una entidad, un libro un usuario. O puede ser una coleccion, una lista.
+- Cada recurso se va a identificar con una Url.
+
+# Verbos `HTTP`
+
+### Se utilizan para definir las `operaciones` que se hacen con los `recursos`.
+
+- `GET`
+- `POST`
+- `DELETE`
+- `CREATE`
+- `PUT`
+- `PATCH`
+
+`CRU D`
+
--- clase-tres/app.js
+++ clase-tres/app.js
@@ -1,10 +1,16 @@
 const express = require("express"); // require --> commonsjs
+const lovecraft = require("./biblioteca.json");
 
 const app = express();
 app.disable("x-powered-by"); // Desabilitar el header X-Powered-by: Express
 
 app.get("/", (req, res) => {
   res.json({ message: "Ejemplo de string" });
+});
+
+// Todos los recursos que sean LOVECRAFT se identifican con /lovecraft
+app.get("/lovecraft", (req, res) => {
+  res.json(lovecraft);
 });
 
 const PORT = process.env.PORT ?? 1234;
