765b9e6e06c8709f10b1df765bbe7c4b51caa687
gramaphenia <dicoratojuanpablo@gmail.com>
2023-10-20 11:33:01 -0300

    mods-a-fix

--- clase-tres/app.js
+++ clase-tres/app.js
@@ -1,7 +1,9 @@
 const express = require("express"); // require --> commonsjs
+const crypto = require("node:crypto");
 const lovecraft = require("./biblioteca.json");
 
 const app = express();
+app.use(express.json());
 app.disable("x-powered-by"); // Desabilitar el header X-Powered-by: Express
 
 app.get("/", (req, res) => {
@@ -10,22 +12,46 @@ app.get("/", (req, res) => {
 
 // Todos los recursos que sean LOVECRAFT se identifican con /lovecraft
 app.get("/lovecraft", (req, res) => {
-  const {genero} = req.query
+  const { genero } = req.query;
   if (genero) {
-    const filtro = lovecraft.filter (
-      book => book.genero.some(g => g.toLowerCase() === genero.toLowerCase())
-    )
-    return res.json(filtro)
+    const filtro = lovecraft.filter((book) =>
+      book.genero.some((g) => g.toLowerCase() === genero.toLowerCase())
+    );
+    return res.json(filtro);
   }
   res.json(lovecraft);
 });
-
+// Ruta para obtener un libro de Lovecraft por su ID
 app.get("/lovecraft/:id", (req, res) => {
   const { id } = req.params;
   const biblioteca = lovecraft.find((biblioteca) => biblioteca.id === id);
   if (biblioteca) return res.json(biblioteca);
 
   res.status(400).json({ message: "Libro no encontrado" });
+});
+
+app.post("/lovecraft", (req, res) => {
+  const {
+  titulo,
+  genero,
+  año,
+  descripcion,
+  foto_de_portada, } = req.body;
+
+  // Crear un nuevo libro con un UUID v4 como ID
+  const nuevoLibro = {
+    id: crypto.randomUUID(), //uuid v4
+    titulo,
+    genero,
+    año,
+    descripcion,
+    foto_de_portada,
+  };
+
+  lovecraft.push(nuevoLibro);
+
+  res.status(201).json(nuevoLibro);
+
 });
 
 const PORT = process.env.PORT ?? 1234;
